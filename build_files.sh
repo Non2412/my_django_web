7d104dc28cec84391d00fa11d2c906aa872fa062
Non2412 <ctf2547@gmail.com>
2024-08-13 14:31:56 +0700

    config

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
-pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput --clearpip install -r requirements.txt
+echo "Building the project..."
+python3.12 -m pip install -r requirements.txt
+
+echo "Collect Static..."
 python3.12 manage.py collectstatic --noinput --clear
